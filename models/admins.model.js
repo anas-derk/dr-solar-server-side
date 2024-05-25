fe592bc6dcca8da5d93f0286e8a5c49e4f38c994
anas derk <anas.derk2020@gmail.com>
2024-05-25 05:33:48 +0300

    Continue With Fix

--- models/admins.model.js
+++ models/admins.model.js
@@ -1,169 +1,65 @@
-// استيراد كائن ال userModel
+// استيراد كائن ال adminModel
 
-const { userModel, adminModel } = require("./all.models");
+const { adminModel, userModel } = require("./all.models");
 
-// استيراد مكتبة تشفير كلمة المرور
+// استيراد مكتبة التشفير
 
 const { compare, hash } = require("bcryptjs");
 
-// تعريف دالة إنشاء حساب مستخدم جديد
-
-async function createNewUser(userInfo) {
-    try {
-        let user;
-        // التحقق فيما إذا كان الإيميل قد تمّ إرساله أم لا
-        if (userInfo.email.length === 0) {
-            // التحقق من أنّ رقم الموبايل موجود
-            user = await userModel.findOne({ mobilePhone: userInfo.mobilePhone });
-        } else {
-            // إذا تم إرسال الإيميل وكلمة المرور بالتالي نتحقق من كون هذا المستخدم موجود مسبقاً
-            user = await userModel.findOne({
-                $or: [
-                    {
-                        email: userInfo.email,
-                    },
-                    {
-                        mobilePhone: userInfo.mobilePhone,
-                    }
-                ]
-            });
-        }
-        // التحقق من أنّ المستخدم موجود بعد البحث عنه في قاعدة البيانات
-        if (user) {
+async function adminLogin(email, password) {
+    try{
+        // البحث في جدول المسؤولين عن إيميل مطابق
+        const adminData = await adminModel.findOne({ email });
+        // في حالة لم يكن يوجد بيانات لهذا الإيميل نرجج رسالة خطأ
+        if (!adminData) {
             return {
-                msg: "عذراً لا يمكن إنشاء الحساب لأنه موجود مسبقاً !!",
+                msg: "عذراً البريد الالكتروني أو كلمة السر خاطئة ...",
                 error: true,
-                data: {}
+                data: {},
+            }
+        }
+        // في حالة كانت توجد بيانات لهذا الإيميل نتحقق من كلمة السر هل صحيحية أم لا
+        const isTruePassword = await compare(password, adminData.password);
+        // في حالة كانت صحيحة نرجع رقم معرّف المسؤول
+        if (isTruePassword) {
+            return {
+                msg: "تم تسجيل الدخول بنجاح !!",
+                error: false,
+                data: {
+                    _id: adminData._id
+                }
             };
         }
-        // تشفير كلمة المرور بما أنّ المستخدم غير موجود مسبقاً
-        const encrypted_password = await hash(userInfo.password, 10);
-        // إنشاء مستخدم جديد في جدول المستخدمين
-        const newUser = new userModel({
-            firstAndLastName: userInfo.firstAndLastName,
-            email: userInfo.email,
-            mobilePhone: userInfo.mobilePhone,
-            password: encrypted_password,
-            gender: userInfo.gender,
-            birthday: userInfo.birthday,
-            city: userInfo.city,
-            address: userInfo.address,
-        });
-        // حفظ المستخدم الجديد في جدول المستخدمين
-        await newUser.save();
+        // في حالة لم تكن صحيحة نرجع رسالة خطأ
         return {
-            msg: "تم بنجاح إنشاء الحساب",
-            error: false,
+            msg: "عذراً البريد الالكتروني أو كلمة السر خاطئة ...",
+            error: true,
             data: {}
         }
     }
-    catch (err) {
+    catch(err) {
         // في حالة حدث خطأ أثناء العملية ، نرمي استثناء بالخطأ
         throw Error(err);
     }
 }
 
-async function isUserAccountExist(email) {
+async function getAdminInfo(adminId) {
     try {
-        // التحقق من أن المستخدم موجود في جدول المستخدمين
-        const user = await userModel.findOne({ email });
-        if (user) {
-            // في حالة كان موجوداً ، نقطع الاتصال بقاعدة البيانات ونعيد بيانات هذا المستخدم المطلوبة فقط
-            return {
-                msg: "عملية  التحقق من وجود الحساب تمت بنجاح !!",
-                error: false,
-                data: {
-                    userId: user._id,
-                    userType: "user"
-                }
-            }
-        }
-        // في حالة لم يكن موجوداً في جدول المستخدمين عندها نبحث في جدول المسؤولين
-        const admin = await userModel.findOne({ email });
+        // التحقق من أنّ المستخدم موجود عن طريق البحث في جدول المسؤولين عن بيانات رقم معرّف المسؤول
+        const admin = await adminModel.findById(adminId);
+        // في حالة كانت توجد بيانات لهذا المسؤول عندها نُرجع بياناته
         if (admin) {
-            // في حالة كان موجوداً ، نقطع الاتصال بقاعدة البيانات ونعيد بيانات هذا المستخدم المطلوبة فقط
             return {
-                msg: "عملية  التحقق من وجود الحساب تمت بنجاح !!",
+                msg: "عملية جلب بيانات المسؤول تمت بنجاح !!",
                 error: false,
-                data: {
-                    userId: admin._id,
-                    userType: "admin"
-                }
+                data: admin
             }
         }
-        // في حالة لم يكن موجوداً في جدول المسؤولين عندها نعيد false للدلالة أنّ المستخدم غير موجود نهائياً
+        // في حالة لم توجد بيانات نُرجع رسالة خطأ
         return {
-            msg: "عذراً ، هذا الحساب غير موجود !!",
+            msg: "عذراً ، حساب المسؤول غير موجود",
             error: true,
             data: {}
-        };
-    } catch (err) {
-        // في حالة حدث خطأ أثناء العملية ، نرمي استثناء بالخطأ
-        throw Error(err);
-    }
-}
-
-async function login(text, password) {
-    try {
-        // التحقق من كون الحساب موجود في قاعدة البيانات أم لا عن طريق البحث عن إيميل مطابق أو رقم هاتف مطابق
-        const user = await userModel.findOne({
-            $or: [
-                {
-                    email: text
-                },
-                {
-                    mobilePhone: text
-                }
-            ]
-        });
-        // التحقق من كون يوجد مستخدم فعلياً أي يوجد كائن يحوي بيانات المستخدم
-        if (user) {
-            // التحقق من كلمة السر صحيحة أم لا لأنّ المستخدم موجود
-            const isTruePassword = await compare(password, user.password);
-            // في حالة كلمة السر صحيحة نعيد معرّف المستخدم أو نعيد رسالة خطأ في حالة لم تكن صحيحة
-            if (isTruePassword) {
-                return {
-                    msg: "عملية تسجيل الدخول تمت بنجاح !!",
-                    error: false,
-                    data: {
-                        _id: user._id
-                    }
-                }
-            }
-            return {
-                msg: "عذراً ، الإيميل أو رقم الهاتف خاطئ أو كلمة السر خاطئة",
-                error: true,
-                data: {}
-            }
-        }
-        return {
-            msg: "عذراً ، الإيميل أو رقم الهاتف خاطئ أو كلمة السر خاطئة",
-            error: true,
-            data: {}
-        }
-    }
-    catch (err) {
-        // في حالة حدث خطأ أثناء العملية ، نرمي استثناء بالخطأ
-        throw Error(err);
-    }
-}
-
-async function getUserInfo(userId) {
-    try {
-        // التحقق من كون المستخدم موجود في قاعدة البيانات عن طريق معرّفه
-        const user = await userModel.findById(userId);
-        // قطع الاتصال بقاعدة البيانات وفي حالة كان موجوداً نعيد بيانات وإلا نعيد رسالة خطأ
-        if (user) {
-            return {
-                msg: "عملية جلب بيانات المستخدم تمت بنجاح !!",
-                error: false,
-                data: user
-            }
-        }
-        return {
-            msg: "عذراً ، المستخدم غير موجود",
-            error: false,
-            data: user
         }
     } catch (err) {
         // في حالة حدث خطأ أثناء العملية ، نرمي استثناء بالخطأ
@@ -171,224 +67,36 @@ async function getUserInfo(userId) {
     }
 }
 
-async function updateProfile(userId, newUserData, isSameOfEmail, isSameOfMobilePhone) {
+async function resetPasswordForUserFromAdmin(adminId, mobilePhone) {
     try {
-        let user;
-        // التحقق من كون قد تمّ إرسال تفس رقم الموبايل وليس نفس الإيميل من أجل عدم البحث في جدول المستخدمين بواسطة رقم الهاتف لأننا لا نريد تغيير رقم الهاتف الافتراضي
-        if (isSameOfEmail === "no" && isSameOfMobilePhone == "yes") {
-            // التحقق من أن المستخدم موجود عن طريق البحث في جدول المستخدمين عن إيميل مطابق
-            user = await userModel.findOne({ email: newUserData.email });
-            if (user) {
+        const admin = await adminModel.findById(adminId);
+        if (admin) {
+            // التحقق من أنّّه يوجد مستخدم له نفس رقم الموبايل في جدول المستخدمين
+            const user = await userModel.findOne({ mobilePhone });
+            // إذا لم يكن هنالك مستخدم مطابق عندها نرجع رسالة خطأ
+            if (!user) {
                 return {
-                    msg: "عذراً لا يمكن تعديل بيانات الملف الشخصي لأن البريد الإلكتروني أو رقم الموبايل موجود مسبقاً !!",
+                    msg: "عذراً ، الحساب غير موجود",
                     error: true,
                     data: {}
                 }
-            } else {
-                // في حالة لم يكن هنالك إيميل مطابق ، نتأكد من كون كلمة السر قد تمّ إرسالها من أجل تغييرهاأم لا
-                if (newUserData.password !== "") {
-                    // تشفير كلمة السر
-                    const newEncryptedPassword = await hash(newUserData.password, 10);
-                    // تعديل البيانات المرسلة فقط أي بدون رقم الهاتف
-                    await userModel.updateOne({ _id: userId }, {
-                        firstAndLastName: newUserData.firstAndLastName,
-                        email: newUserData.email.toLowerCase(),
-                        password: newEncryptedPassword,
-                        gender: newUserData.gender,
-                        birthday: newUserData.birthday,
-                        city: newUserData.city,
-                        address: newUserData.address,
-                    });
-                    return {
-                        msg: "عملية تحديث بيانات المستخدم تمت بنجاح !!",
-                        error: false,
-                        data: {}
-                    }
-                }
-                else {
-                    // تعديل بيانات المستخدم لكن بدون كلمة السر ورقم الهاتف
-                    await userModel.updateOne({ _id: userId }, {
-                        firstAndLastName: newUserData.firstAndLastName,
-                        email: newUserData.email.toLowerCase(),
-                        gender: newUserData.gender,
-                        birthday: newUserData.birthday,
-                        city: newUserData.city,
-                        address: newUserData.address,
-                    });
-                    return {
-                        msg: "عملية تحديث بيانات المستخدم تمت بنجاح !!",
-                        error: false,
-                        data: {}
-                    }
-                }
             }
-            // التحقق من أنّ الإيميل نفسه ورقم الموبايل ليس نفسه
-        } else if (isSameOfEmail == "yes" && isSameOfMobilePhone == "no") {
-            // البحث في جدول المستخدمين عن مستخدم له نفس رقم الموبايل لأنه غير مسموح تكرار رقم الموبايل
-            user = await userModel.findOne({ mobilePhone: newUserData.mobilePhone });
-            if (user) {
-                return {
-                    msg: "عذراً لا يمكن تعديل بيانات الملف الشخصي لأن البريد الإلكتروني أو رقم الموبايل موجود مسبقاً !!",
-                    error: true,
-                    data: {}
-                };
-            } else {
-                // التحقق من أنّ السر غير مرسلة كي لا يتم تعديلها مع بيانات المستخدم
-                if (newUserData.password !== "") {
-                    // تشفير كلمة السر
-                    const newEncryptedPassword = await hash(newUserData.password, 10);
-                    // تعديل بيانات المستخدم بدون الإيميل
-                    await userModel.updateOne({ _id: userId }, {
-                        firstAndLastName: newUserData.firstAndLastName,
-                        mobilePhone: newUserData.mobilePhone,
-                        password: newEncryptedPassword,
-                        gender: newUserData.gender,
-                        birthday: newUserData.birthday,
-                        city: newUserData.city,
-                        address: newUserData.address,
-                    });
-                    return {
-                        msg: "عملية تحديث بيانات المستخدم تمت بنجاح !!",
-                        error: false,
-                        data: {}
-                    }
-                } else {
-                    // تعديل بيانات المستخدم بدون الإيميل وكلمة السر
-                    await userModel.updateOne({ _id: userId }, {
-                        firstAndLastName: newUserData.firstAndLastName,
-                        mobilePhone: newUserData.mobilePhone,
-                        gender: newUserData.gender,
-                        birthday: newUserData.birthday,
-                        city: newUserData.city,
-                        address: newUserData.address,
-                    });
-                    return {
-                        msg: "عملية تحديث بيانات المستخدم تمت بنجاح !!",
-                        error: false,
-                        data: {}
-                    }
-                }
-            }
-        // التحقق من أنّ الإيميل ورقم الهاتف ليس نفسه
-        } else if (isSameOfEmail == "no" && isSameOfMobilePhone == "no") {
-            // البحث في جدول المستخدمين عن مستخدم له إيميل أو رقم هاتف مطابق
-            user = await userModel.findOne({
-                $or: [
-                    {
-                        email: newUserData.email
-                    },
-                    {
-                        mobilePhone: newUserData.mobilePhone,
-                    }
-                ]
+            // قي حالة كان يوجد مستخدم مطابق فإننا نقوم بتشفير رقم الهاتف لجعله هو نفسه كلمة السر
+            const newEncryptedPassword = await hash(mobilePhone, 10);
+            // تعديل كلمة السر الخاصة بالمستخدم الذي له نفس رقم الموبايل لتصبح نفسها رقم الموبايل
+            await userModel.updateOne({ mobilePhone }, {
+                password: newEncryptedPassword,
             });
-            if (user) {
-                return {
-                    msg: "عذراً لا يمكن تعديل بيانات الملف الشخصي لأن البريد الإلكتروني أو رقم الموبايل موجود مسبقاً !!",
-                    error: true,
-                    data: {}
-                };
-            } else {
-                // التحقق من أنّ كلمة السر قد تمّ إرسالها من أجل تغييرها مع بيانات المستخدم
-                if (newUserData.password !== "") {
-                    // تشفير كلمة السر
-                    const newEncryptedPassword = await hash(newUserData.password, 10);
-                    // تعديل بيانات المستخدم كاملة
-                    await userModel.updateOne({ _id: userId }, {
-                        firstAndLastName: newUserData.firstAndLastName,
-                        email: newUserData.email,
-                        mobilePhone: newUserData.mobilePhone,
-                        password: newEncryptedPassword,
-                        gender: newUserData.gender,
-                        birthday: newUserData.birthday,
-                        city: newUserData.city,
-                        address: newUserData.address,
-                    });
-                    return {
-                        msg: "عملية تحديث بيانات المستخدم تمت بنجاح !!",
-                        error: false,
-                        data: {}
-                    }
-                } else {
-                    // تعديل بيانات المستخدم كاملة بدون كلمة السر
-                    await userModel.updateOne({ _id: userId }, {
-                        firstAndLastName: newUserData.firstAndLastName,
-                        email: newUserData.email,
-                        mobilePhone: newUserData.mobilePhone,
-                        gender: newUserData.gender,
-                        birthday: newUserData.birthday,
-                        city: newUserData.city,
-                        address: newUserData.address,
-                    });
-                    return {
-                        msg: "عملية تحديث بيانات المستخدم تمت بنجاح !!",
-                        error: false,
-                        data: {}
-                    }
-                }
-            }
-            // في حالة لم تكن من ضمن الاحتمالات السابقة أي لم يتم تعديل لا اإيميل ولا رقم الهاتف
-        } else {
-            // التحقق من أنّه قد تمّ إرسال كلمة السر لتعديلها
-            if (newUserData.password !== "") {
-                // تشفير كلمة السر
-                const newEncryptedPassword = await hash(newUserData.password, 10);
-                // تعديل بيانات السمتخدم بدون الإيميل أو رقم الموبايل
-                await userModel.updateOne({ _id: userId }, {
-                    firstAndLastName: newUserData.firstAndLastName,
-                    password: newEncryptedPassword,
-                    gender: newUserData.gender,
-                    birthday: newUserData.birthday,
-                    city: newUserData.city,
-                    address: newUserData.address,
-                });
-                return {
-                    msg: "عملية تحديث بيانات المستخدم تمت بنجاح !!",
-                    error: false,
-                    data: {}
-                }
-            } else {
-                // تعديل بيانات المستخدم بدون الإيميل و رقم الموبايل وكلمة السر
-                await userModel.updateOne({ _id: userId }, {
-                    firstAndLastName: newUserData.firstAndLastName,
-                    gender: newUserData.gender,
-                    birthday: newUserData.birthday,
-                    city: newUserData.city,
-                    address: newUserData.address,
-                });
-                return {
-                    msg: "عملية تحديث بيانات المستخدم تمت بنجاح !!",
-                    error: false,
-                    data: {}
-                }
-            }
-        }
-    }
-    catch (err) {
-        // في حالة حدث خطأ أثناء العملية ، نرمي استثناء بالخطأ
-        throw Error(err);
-    }
-}
-
-async function resetUserPassword(email, newPassword, userType) {
-    try {
-        // تشفير كلمة السر
-        const newEncryptedPassword = await hash(newPassword, 10);
-        // التحقق من كون نوع المستخدم هو مستخدم عادي
-        if (userType == "user") {
-            // إعادة تعيين كلمة السر من خلال تعديل بيانات المستخدم في جدول المستخدمين ومن ثمّ إعادة رسالة نجاح
-            await userModel.updateOne({ email }, { password: newEncryptedPassword });
+            // في حالة نجاح العملية فإننا نعيد رسالة نجاح
             return {
-                msg: "لقد تمّت عملية إعادة تعيين كلمة المرور الخاصة بك بنجاح !!",
+                msg: "تهانينا ، لقد تمّ إعادة تعيين كلمة السر لتصبح على نفس رقم الموبايل",
                 error: false,
                 data: {}
             }
         }
-        // إعادة تعيين كلمة السر من خلال تعديل بيانات المستخدم في جدول المسؤولين في حالة لم يكن مستخدم عادي ومن ثمّ إعادة رسالة نجاح
-        await adminModel.updateOne({ email }, { password: newEncryptedPassword });
         return {
-            msg: "لقد تمّت عملية إعادة تعيين كلمة المرور الخاصة بك بنجاح !!",
-            error: false,
+            msg: "عذراً ، حساب المسؤول غير موجود",
+            error: true,
             data: {}
         }
     } catch (err) {
@@ -397,12 +105,9 @@ async function resetUserPassword(email, newPassword, userType) {
     }
 }
 
-// تصدير الدوال المُعرّفة
+// تصدير الدوال المعرّفة سابقاً
 module.exports = {
-    createNewUser,
-    login,
-    getUserInfo,
-    updateProfile,
-    isUserAccountExist,
-    resetUserPassword,
+    adminLogin,
+    getAdminInfo,
+    resetPasswordForUserFromAdmin,
 }
